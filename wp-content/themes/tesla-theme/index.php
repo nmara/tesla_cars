6ab0aa0477a218a77d0dcf41694152a982f9798a
nmara <aga.czekala89@gmail.com>
2018-02-21 00:31:14 +0100

    pricing table - draft

--- wp-content/themes/tesla-theme/index.php
+++ wp-content/themes/tesla-theme/index.php
@@ -1,15 +1,47 @@
 <?php get_header(); ?>
 
+<section id="tesla-cars">
 
-	<footer>
-		<div class="footer__container">
-			<div class="fc container">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-small.png" alt="" />
-				<p>
-					Tesla &#169;2018 all rights reserved
-				</p>
-			</div>
-		</div>
-	</footer>
+    <div class="tesla-cars container">
+
+        <?php if ( have_posts() ) :
+        while (	have_posts() ) :
+        the_post(); ?>
+
+        <?php $features = get_post_meta( get_the_ID(), 'feature' ); ?>
+        <?php $button_label = get_post_meta( get_the_ID(), 'button-label', true ); ?>
+        <?php $additional_info = get_post_meta( get_the_ID(), 'additional-info', true ); ?>
+        <div class="pricing-table">
+
+            <div class="pricing-table__header">
+                <h2 class="model-title"> <?php the_title(); ?> </h2>
+                <div class="separation"></div>
+                <?php the_post_thumbnail(); ?>
+				<!-- <img
+				src="<?php	echo	$img['url'];	?>"
+				alt="<?php	echo	$img['alt'];	?>"> -->
+            </div>
+
+            <div class="pricing-table__content">
+                <p class="pricing-table__content__features">
+                    <?php
+                    foreach ($features as $value) {
+                        echo "$value <br>";
+                    }
+                    ?>
+                </p>
+                <div class="separation"></div>
+                <p class="pricing-table__content__add-info"><?php echo $additional_info ?></p>
+                <a href="#" class="pricing-table__btn"><?php echo $button_label ?></a>
+            </div>
+
+        </div>
+    <?php endwhile; ?>
+    <?php else: ?>
+        <p>Nie	ma	takiego	postu</p>
+    <?php endif; ?>
+    </div>
+
+</section>
 
 <?php get_footer(); ?>
